59c32aa617f675bc93ed7a1624fd3a4262428ccf
gzr2017 <2125561933@qq.com>
2018-10-20 02:03:18 +0900

    初始化页面

---
+++ js/fan.js
@@ -0,0 +1,14 @@
+var roan = 0;
+var img = document.getElementById('container');
+if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function (event) {//监听设备陀螺仪
+        if (-45 < event.gamma && event.gamma < 45)
+            roan = ((event.gamma) + 45) / 2;
+        $("#img").rotate({
+            angle: roan,
+            center: ["39.2%", "72%"],
+        })
+    });
+} else {
+    M.toast({ html: '你的浏览器不支持哦~用移动端试试吧！', classes: 'rounded' });
+}
