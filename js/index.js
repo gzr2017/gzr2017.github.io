98b03ef83a46314647740a6f7ff7edeab51d0d38
gzr1997 <2125561933@qq.com>
2018-12-18 21:57:08 +0900

    首页基本完成

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('.tabs').tabs();
+    $('.owl-carousel').owlCarousel();
 });
-
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: true,
+    draggable: true,
+    freeScroll: true
+});
