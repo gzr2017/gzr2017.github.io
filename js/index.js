b8ed34ac8e1b94c8504e156c204e87bc4708d019
gzr1997 <2125561933@qq.com>
2018-12-18 16:19:44 +0900

    增加对话框

--- js/index.js
+++ js/index.js
@@ -1,28 +1,4 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    navText: [
-        "<i class='fa fa-caret-left'></i>",
-        "<i class='fa fa-caret-right'></i>"
-    ],
-    autoplay: true,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-})
-
-$(document).ready(function () {
-    $('.materialboxed').materialbox();
-    $('.sidenav').sidenav();
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
 });
-
